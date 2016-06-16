c7353dc9776941bd7b4c024aba46f689f1703b8a
Brandon Kase <bkase@cmu.edu>
2016-06-15 17:46:08 -0700

    Cache is profunctor + contravariant
    Profunctor w.r.t. values
    Contravariant functor w.r.t. keys

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,26 @@
 module Main where
 
-import Prelude ((<>), Unit, unit, pure, class Semigroup, bind, (*>), id, const, ($), (<$>), (<<<))
+import Prelude ((<>), Unit, unit, pure, class Semigroup, bind, (*>), id, const, ($), (<$>), (<<<), flip, (+))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
 import Data.Monoid (class Monoid, mempty)
+import Data.Profunctor
+import Data.Functor.Contravariant (class Contravariant, (>$<))
+import Data.Functor
 import Control.MonadPlus (class MonadPlus, empty, (<|>))
 
-data Cache m k v = Cache { get :: k -> m v
-                         , set :: k -> v -> m Unit }
+type Cache m k v = Cache_ m k v v
 
-instance semigroupCache :: (MonadPlus m) => Semigroup (Cache m k v) where
+data Cache_ m k v w = Cache { get :: k -> m w
+                            , set :: k -> v -> m Unit }
+
+data Flipped p (m :: * -> *) v w k = Flipped (p m k v w)
+
+unflip :: forall p m v w k. (Functor m) => Flipped p m v w k -> p m k v w
+unflip (Flipped c) = c
+
+instance semigroupCache :: (MonadPlus m) => Semigroup (Cache_ m k v v) where
   append (Cache{get, set}) (Cache{get: get', set: set'}) =
     Cache
     { get: \k -> get k <|> do
@@ -21,28 +31,26 @@ instance semigroupCache :: (MonadPlus m) => Semigroup (Cache m k v) where
               set k v *> set' k v
     }
 
-instance monoidCache :: (MonadPlus m) => Monoid (Cache m k v) where
+instance monoidCache :: (MonadPlus m) => Monoid (Cache_ m k v v) where
   mempty = Cache { get: const empty
                  , set: const $ const $ pure unit }
 
-lcontramap :: forall p k k' v. (ContravariantInvariant p) => (k' -> k) -> p k v -> p k' v
-lcontramap f = contrainvmap f id id
-
-rinvmap :: forall p k v v'. (ContravariantInvariant p) => (v' -> v) -> (v -> v') -> p k v -> p k v'
-rinvmap = contrainvmap id
-
-class ContravariantInvariant (p :: * -> * -> *) where
-  contrainvmap :: forall k k' v v'. (k' -> k) -> (v' -> v) -> (v -> v') -> p k v -> p k' v'
-  
-instance contravariantInvariantCache :: (MonadPlus m) => ContravariantInvariant (Cache m) where
-  contrainvmap contrak contrav cov (Cache{get, set}) = 
+instance profunctorCache :: (Functor m) => Profunctor (Cache_ m k) where
+  dimap f g (Cache{get, set}) =
     Cache
-    { get: \k -> cov <$> (get $ contrak k)
-    , set: \k -> \v -> set (contrak k) (contrav v)
+    { get: \k -> g <$> get k
+    , set: \k -> \v -> set k (f v)
     }
 
+instance contravariantCache :: (Functor m) => Contravariant (Flipped Cache_ m v w) where
+  cmap f (Flipped(Cache{get, set})) =
+    Flipped $ Cache { get: get <<< f
+                    , set: \k -> \v -> set (f k) v
+                    }
+
 compoundCache :: forall m. (MonadPlus m) => Cache m Int String
-compoundCache = mempty <> mempty
+compoundCache = (unflip ((_+1) >$< Flipped(mempty))) <> dimap vf vf mempty
+  where vf = (_ <> "!")
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
